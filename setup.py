9606308f3173996e2672bb5f02d199a3794d4b71
Martin Wygas <martinwygas@gmai..com>
2021-08-19 16:00:09 +0200

    added/changed some information about the package

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup
 
+description_text = 'This project serves as an Vosk interface for Opencast. It allows to generate subtitles (WebVVT files) from video and audio sources via Vosk.'
+
 setup(name='vosk_cli',
       version='0.1',
-      description='TODO',
-      url='http://github.com/opencast',
+      description=description_text,
+      long_description=description_text,
+      url='https://github.com/elan-ev/vosk-cli',
       author='Martin Wygas',
       author_email='mwygas@uos.de',
       license='Apache-2.0',
       packages=['vosk_cli'],
       license_files = ('LICENSE'),
       install_requires=[
-		'vosk>=0.3.30',
-		'webvtt-py>=0.4.6'
+		    'vosk>=0.3.30',
+		    'webvtt-py>=0.4.6'
       ],
       include_package_data = True,
       zip_safe=False,
